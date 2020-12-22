ed073ab8d320e5af21663375ea5a2d0c437e030a
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2020-12-22 21:44:22 +0300

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,7 @@
+#Homework1
+a = int(input("Num1 = "))
+b = int(input("Num2 = "))
+c = int(input("Num3 = "))
+d = int(input("Num4 = "))
+e = input("String = ")
+print(f"Num1: {a}\nNum2: {b}\nNum3: {c}\nNum4: {d}\nString: {e}")
