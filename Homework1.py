9ccc2036f93adc29cef044e5ac3a055969f70f8c
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2020-12-24 23:40:55 +0300

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,11 @@
+#Homework1
+value1 = input("Value(String1) : ")
+value2 = int(input("Value(Int1): "))
+value3 = float(input("Value(Float): "))
+value4 = input("Value(String2) :")
+value5 = int(input("Value(Int2):"))
+print(f"""Value(String1):{value1}, Type:{type(value1)}
+Value(Int1)   :{value2}, Type:{type(value2)}
+Value(Float)  :{value3}, Type:{type(value3)}
+Value(String2):{value4}, Type:{type(value4)}
+Value(Int2)   :{value5}, Type:{type(value5)}""")
