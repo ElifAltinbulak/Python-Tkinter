29fd570c00eb0fdfe9e6dd2572eae84256cd3d22
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2020-12-31 01:37:01 +0300

    Add files via upload

---
+++ kosul_deyimi.py
@@ -0,0 +1,12 @@
+while True:
+    yas = int(input("Yaşınızı giriniz: "))
+    if 0<yas<=50:
+        print("Yaşınız 0-50 arasındadır.")
+        break
+    elif 50<yas<=100:
+        print("Yaşınız 50-100 arasındadır.")
+        break
+    else:
+        print("Lütfen 0-100 arası bir değer giriniz.")
+        
+            
