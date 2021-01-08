e42754e5963c00f90f5a103f162445e73af4c5ab
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-01-09 01:24:08 +0300

    Update sayıtahmin.py

--- sayıtahmin.py
+++ sayıtahmin.py
@@ -1,17 +1,16 @@
 #Sayı Tahmin
-import random as rnd
-for i in range(0,3):
-    while True:
-        num = int(input("Please enter the integer number: "))
-        sn = rnd.randint(0,10)
-        if num<sn:
-            print("Büyük bir sayı giriniz")
-        elif num>sn:
-            print("Küçük bir sayı giriniz")
-        elif num==sn:
-            print("Buldunuzzzz")
-            break
-        else:
-            print("Geçersiz bir sayı girdiniz")
+ hak=0
+while hak<5:#hakları burda bildir
+    num=int(input("Bir sayı giriniz: "))
+    sn=rnd.randint(0,100)
+    if num==sn:
+        print("Bildiniz")
+        break
+    elif num>sn:
+        print("aşağı")
+    elif num<sn:
+        print("yukarı")
+    hak+=1   
+print(f"Hakkınız bitti sayınız:{sn}")
 
     
