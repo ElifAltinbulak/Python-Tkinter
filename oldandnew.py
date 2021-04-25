69c42d995eef473c5eefa12898793aaa548b6f96
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-04-26 00:36:47 +0300

    Add files via upload

---
+++ oldandnew.py
@@ -0,0 +1,8 @@
+old=int(input("Please enter the number of old videos: "))
+new=int(input("Please enter the number of new videos: "))
+num1=(old*2)
+num2=(new*3)
+result=num1+num2
+print(f"""Your total for the old videos is {num1}$
+Your total for the new videos is {num2}$
+Your total of all videos is {result}$""")
